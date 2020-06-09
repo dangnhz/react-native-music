0a7164853af6b01111c9efdf0b46a2654c746ffb
Dang Nhz <41246555+dangnhz@users.noreply.github.com>
2020-06-09 15:17:26 +1000

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text>Hello</Text>
+    </NavigationContainer>
+  );
+}
